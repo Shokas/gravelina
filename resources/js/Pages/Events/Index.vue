a88f401d497242b6eb0e9589fd75f2aec5687151
Žilvinas Gadliauskas <zilvinas.gadliauskas@bite.lt>
2023-12-13 00:52:58 +0200

    show more data in event list

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -13,7 +13,9 @@ import AppLayout from '@/Layouts/AppLayout.vue';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                     <li v-for="event in events" :key="event.id">
-                        <a :href="`/events/${event.id}`">{{ event.name }}</a>
+                        <a :href="`/events/${event.id}`">
+                            {{ event.name }} | {{ event.start_date }} | {{ event.distance }} | {{ event.type }}
+                        </a>
                     </li>
                 </div>
             </div>
